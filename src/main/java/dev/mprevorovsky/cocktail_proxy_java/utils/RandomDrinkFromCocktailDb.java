a8744d3c27ea5fe8e9694f31f50c36779936a9b9
mprevorovsky <mprevorovsky@volny.cz>
2023-11-22 19:42:51 +0100

    corrected documentation

--- src/main/java/dev/mprevorovsky/cocktail_proxy_java/utils/RandomDrinkFromCocktailDb.java
+++ src/main/java/dev/mprevorovsky/cocktail_proxy_java/utils/RandomDrinkFromCocktailDb.java
@@ -1,15 +1,14 @@
 package dev.mprevorovsky.cocktail_proxy_java.utils;
 
-/**
- * Retrieves data for a random drink from the CocktailDB.
- */
-
 import dev.mprevorovsky.cocktail_proxy_java.model.CocktailDbRecord;
 import dev.mprevorovsky.cocktail_proxy_java.model.Drink;
 import org.springframework.web.client.RestTemplate;
 
 import static dev.mprevorovsky.cocktail_proxy_java.CocktailProxyJavaApplication.cocktailDbApiRandomDrinkUrl;
 
+/**
+ * Retrieves data for a random drink from the CocktailDB.
+ */
 public class RandomDrinkFromCocktailDb {
 
     public static Drink getRandomDrinkFromCocktailDb(RestTemplate restTemplate) {
